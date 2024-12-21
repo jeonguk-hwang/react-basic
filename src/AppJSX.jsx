38c069548fff4e592be4205503a2abda5739d159
jeonguk-hwang <jeonguk.hwang@hatchling.kr>
2024-12-21 16:57:36 +0900

    feat: Add initial Profile component - First step complete

---
+++ src/AppJSX.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+
+function AppJSX() {
+  const name = '엘리';
+  const list = ['우유', '딸기', '바나나', '요거트'];
+  return (
+    <>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
+      <h2>Hello!</h2>
+      <p>{name}</p>
+      <ul>
+        {list.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+      <img
+        style={{ width: '200px', height: '200px' }}
+        src='https://images.unsplash.com/photo-1719937051230-8798ae2ebe86?q=80&w=2944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' 
+        alt='nature' 
+      />
+    </>
+  );
+}
+
+export default AppJSX;
